3f4aacd336e450b990abf465563560f3a0f32e1d
cflorian <cflorian@SISTEMAS40>
2024-05-24 21:06:27 -0500

    Organizar Tablero

--- Quiz/Quiz/Program.cs
+++ Quiz/Quiz/Program.cs
@@ -8,8 +8,27 @@ class Program
     static int posY;
     static int tamano;
 
-    static void main(string[] args)
+    static void Main(string[] args)
     {
+        InicializarJuego();
 
-        inicializarJuego();
-    }
+        bool salir = false;
+        while (!salir) 
+        {
+            // ORGANIZAR TABLERO
+
+            MostrarTablero();
+
+            Console.WriteLine("Puntos:" + puntaje);
+            Console.WriteLine("Selecciona una opcion");
+            Console.WriteLine("1. Flecha Derecha");
+            Console.WriteLine("2. Flecha Izquierda");
+            Console.WriteLine("3. Flecha Arriba");
+            Console.WriteLine("4. Flecha Abajo");
+            Console.WriteLine("Tecla Esc Para salir del juego");
+
+
+
+
+
+        }
